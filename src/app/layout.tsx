d579df57732701327bf47dc5e953e7a4211cb8d6
yasuwotaku <yasuda295@gmail.com>
2025-02-12 21:49:19 +0900

    feat: 一旦darkmodeは考慮しない

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { CMS_NAME, HOME_OG_IMAGE_URL } from "@/lib/constants";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import cn from "classnames";
-import { ThemeSwitcher } from "./_components/theme-switcher";
 
 import "zenn-content-css";
 import "./globals.css";
@@ -39,7 +38,6 @@ export default function RootLayout({
 				<script async src="https://embed.zenn.studio/js/listen-embed-event.js"></script>
 			</head>
 			<body className={cn(inter.className, "dark:bg-slate-900 dark:text-slate-400")}>
-				<ThemeSwitcher />
 				<div className="min-h-screen">{children}</div>
 				<Footer />
 			</body>
