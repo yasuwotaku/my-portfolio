1ba08b3709d70499c09e8c44f3a5eff278c43341
yasuwotaku <yasuda295@gmail.com>
2025-03-09 02:17:49 +0900

    feat: フッターの要素を削除

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,6 +1,4 @@
 import { Typography } from "@material-tailwind/react";
-import { Breadcrumb, BreadcrumbLink, BreadcrumbSeparator } from "@material-tailwind/react";
-import { HomeSimple } from "iconoir-react";
 
 const YEAR = new Date().getFullYear();
 
@@ -8,19 +6,9 @@ export function Footer() {
 	return (
 		<footer
 			className={`
-				flex w-full flex-row flex-wrap items-center justify-center gap-x-12 gap-y-3 border-t border-surface py-4 text-center
-				md:justify-between
+				w-full border-t border-surface p-4 text-center
 			`}
 		>
-			<Breadcrumb>
-				<BreadcrumbLink href="/">
-					<HomeSimple className="h-[18px] w-[18px]" />
-				</BreadcrumbLink>
-				<BreadcrumbSeparator />
-				<BreadcrumbLink href="posts">Posts</BreadcrumbLink>
-				<BreadcrumbSeparator />
-				<BreadcrumbLink href="about">About</BreadcrumbLink>
-			</Breadcrumb>
 			<Typography type="small">&copy;yasuworks.com, 2024-{YEAR}</Typography>
 		</footer>
 	);
